68cb5f14f437aaf8a2cdeb71720350b2ba919e1b
Esraa Yahia <esraaelsabahy657@gmail.com>
2026-08-24 18:13:03 +0200

    admin_dashboard

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ECommerce_Mvc.Models;
+using ECommerce_Mvc.Data;
+using ECommerce_Mvc.Constants;
+
+namespace ECommerce_Mvc.Controllers
+{
+    [Authorize(Roles = AuthorizationConstants.ADMINISTRATORS)]
+    public class AdminController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminController(AppDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            int totalUsers = await _userManager.Users.CountAsync();
+            int totalSellers = (await _userManager.GetUsersInRoleAsync(AuthorizationConstants.SELLERS)).Count;
+            int totalAdmins = (await _userManager.GetUsersInRoleAsync(AuthorizationConstants.ADMINISTRATORS)).Count;
+
+            var vm = new AdminDashboardVM
+            {
+                TotalCustomers = totalUsers - totalSellers - totalAdmins,
+                TotalSellers = totalSellers,
+                TotalProducts = await _context.Products.CountAsync(),
+                TotalOrders = await _context.Orders.CountAsync(),
+                PendingOrders = 0 
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ Models/AdminDashboardVM.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce_Mvc.Models
+{
+        public class AdminDashboardVM
+        {
+            public int TotalCustomers { get; set; }
+            public int TotalSellers { get; set; }
+            public int TotalProducts { get; set; }
+            public int TotalOrders { get; set; }
+            public int PendingOrders { get; set; }
+        }
+    }
