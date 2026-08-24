7b8c1810de3db52e21b8d6b9d13a73358211c255
Esraa Yahia <esraaelsabahy657@gmail.com>
2026-08-24 18:57:40 +0200

    Add seller dashboard

---
+++ Models/SellerDashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce_Mvc.Models;
+
+public class SellerDashboardViewModel
+{
+    public int ProductsCount { get; set; }
+
+    public int OrdersCount { get; set; }
+
+    public decimal Sales { get; set; }
+}
