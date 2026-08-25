4e80b89222ece703572dd2c9081b82e4ffe553ed
Esraa Yahia <esraaelsabahy657@gmail.com>
2026-08-25 16:23:56 +0200

    Add product reviews

---
+++ Models/Review.cs
@@ -0,0 +1,25 @@
+﻿namespace ECommerce_Mvc.Models;
+
+public class Review
+{
+    private Review() { } // Required by EF Core
+
+    public int Id { get; private set; }
+    public int Rating { get; private set; }        // 1 -> 5
+    public string Comment { get; private set; } = null!;
+    public DateTime CreatedAtUtc { get; private set; } = DateTime.UtcNow;
+
+    public int ProductId { get; private set; }
+    public Product? Product { get; private set; }
+
+    public string UserId { get; private set; } = null!;
+    public ApplicationUser? User { get; private set; }
+
+    public Review(int productId, string userId, int rating, string comment)
+    {
+        ProductId = productId;
+        UserId = userId;
+        Rating = rating;
+        Comment = comment;
+    }
+}
